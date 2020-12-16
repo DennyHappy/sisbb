21bc77d6dc833257e0d84860d263c849cd228d90
DennyHappy <38359583+DennyHappy@users.noreply.github.com>
2020-12-16 07:55:47 -0300

    Listar Minhas Reservas
    Listar Minhas Reservas
    VIEW + CONTROLLER da página de listagem das reservas do usuário Aluno, com base na sessão iniciada com seu Login no sistema.

--- includes/info_user.php
+++ includes/info_user.php
@@ -6,8 +6,12 @@
                 <h4>Bem-vindo <?=$_SESSION['nome']?></h4>
             </div>
             <div class="col-4 text-right">
+                <p>Matricula: <?=$_GET['mtc']?></p>
                 <p>E-mail: <?=$_SESSION['email']?></p>
-                <a href="logout.php" class="btn btn-info btn-sm col-6">Sair</a>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <a href="ver_minhas_reservas.php?mtc=<?=$_GET['mtc']?>" class="btn btn-info btn-sm">Minhas Reservas</a>
+                    <a href="logout.php" class="btn btn-info btn-sm active">Sair</a>
+                </div>
             </div>
         </div>
     </div>

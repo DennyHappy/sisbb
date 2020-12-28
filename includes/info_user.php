918e01e53a715e96e5255ff47e7745045fbe3262
Denilson Felisberto <38359583+DennyHappy@users.noreply.github.com>
2020-12-28 20:26:40 -0300

    Cadastro de usuario comum concluido

--- includes/info_user.php
+++ includes/info_user.php
@@ -6,7 +6,7 @@
                 <p><strong>Bem-vindo </strong><?=$_SESSION['nome']?></p>
             </div>
             <div class="col-8 text-right">
-                <p><strong>Matricula: </strong><?=$_SESSION['matricula']?></p>
+                <p><strong>Matricula: </strong><?=$_SESSION['mtcl']?></p>
                 <p><strong>E-mail: </strong><?=$_SESSION['email']?></p>
                 <div class="btn-group" role="group" aria-label="Basic example">
                     <a href="index.php" class="btn btn-info btn-sm">
--- sisbbAluno/cadastrarUser.php
+++ sisbbAluno/cadastrarUser.php
@@ -10,42 +10,38 @@ try {
   $_SESSION['email'] = $userProfile->email;
   $_SESSION['identifier'] = $userProfile->identifier;
 
-  if (isset($_POST['matricula'],$_POST['nome'],$_POST['email'],$_POST['identifier'])) {
-
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'http://localhost:8080/usuariocm/',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => '',
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => 'POST',
-      CURLOPT_POSTFIELDS =>'{
-        "matricula": '.$_POST['matricula'].',
-        "nome": "'.$_POST['nome'].'",
-        "email": "'.$_POST['email'].'",
-        "idUser": '.$_POST['identifier'].'
-    }',
-      CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json'
-      ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-
-    //echo $response;
-
-    header('location: index.php?status=successCadastro');
-    exit;
+  if (isset($_POST['matricula'],$_POST['nome'],$_POST['email'],$_POST['idUser'])) {
     
-  }else{
-    echo 'Preencha os campos!';
-  }
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://localhost:8080/usuariocm/',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+          "matricula": '.$_POST['matricula'].',
+          "nome": "'.$_POST['nome'].'",
+          "email": "'.$_POST['email'].'",
+          "idUser": '.$_POST['idUser'].'
+      }',
+        CURLOPT_HTTPHEADER => array(
+          'Content-Type: application/json'
+        ),
+      ));
+
+      $response = curl_exec($curl);
+
+      curl_close($curl);
+      
+      header('location: index.php?status=successCadastroUser');
+      $_SESSION['mtcl'] = $_POST['matricula'];
+      exit;
+    }
   
 }catch( Exception $e ){
     echo $e->getMessage() ;
